7929e93cc99470d99c6ab1ab5502c607b48532c7
zia243 <mziammalik@gmail.com>
2025-01-14 22:44:11 +0500

    account creation + cron job added in webhook

--- app/api/webhook/coinbase/route.ts
+++ app/api/webhook/coinbase/route.ts
@@ -1,12 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 const coinbase = require("coinbase-commerce-node");
-import {createNotification}  from "@/app/api/invoice/create/route";
+import { dateToFullCronString } from "@/lib/utils";
+import { createNotification } from "@/app/api/invoice/create/route";
+import { sendNotification } from "@/helper/notifications";
+import { AccountStatus, AccountType } from "@prisma/client";
 import { connectToDatabase } from "@/lib/database";
 import prisma from "@/prisma/client";
 import { NextApiResponse } from "next";
+
 const { Webhook } = coinbase;
 
 const webhookSecret = process.env.COINBASE_COMMERCE_SHARED_SECRET;
+
+type cronJobTypes = "objectiveMin" | "objectiveMax" | "inactivity";
+interface cronJob {
+  jobName: string;
+  time: string;
+  type: cronJobTypes;
+  accountId: string;
+}
 
 // export const config = {
 //   api: {
@@ -14,9 +26,60 @@ const webhookSecret = process.env.COINBASE_COMMERCE_SHARED_SECRET;
 //   },
 // };
 
-export async function POST(req: NextRequest,res:NextApiResponse) {
+async function createUserAccount(reference: any) {
+  const accountDetails = JSON.parse(reference).accountDetails;
+  const billinDetails = JSON.parse(reference).billingDetails;
+
+  const newAcc = await prisma.$transaction(async (prisma) => {
+    // Step 1: Create the new account
+    const createdAccount = await prisma.account.create({
+      data: {
+        accountType: accountDetails.accountType as AccountType,
+        accountSize: accountDetails.accountSize as string,
+        status: accountDetails.status as AccountStatus,
+        balance: accountDetails.balance as number,
+        accountNumber: accountDetails.accountNumber as string,
+        userId: accountDetails.userId,
+        minBetPeriod: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 7 days from now
+        maxBetPeriod: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+      },
+    });
+
+    // Step 2: Create the billing address linked to the account
+    await prisma.billingAddress.create({
+      data: {
+        address: billinDetails.address,
+        city: billinDetails.city,
+        country: billinDetails.country,
+        email: billinDetails.email,
+        firstName: billinDetails.firstName,
+        lastName: billinDetails.lastName,
+        phone: billinDetails.phone,
+        zipCode: billinDetails.postalCode,
+        state: billinDetails.state,
+        accountId: createdAccount.id, // Link to the newly created account
+      },
+    });
+
+    return createdAccount; // Return the created account
+  });
+
   try {
-    const rawBody = await req.text(); 
+    await sendNotification(
+      "Account created successfully",
+      "UPDATE",
+      accountDetails.userId
+    );
+  } catch (error) {
+    console.error("Error sending notification:", error);
+  }
+
+  return newAcc;
+}
+
+export async function POST(req: NextRequest, res: NextApiResponse) {
+  try {
+    const rawBody = await req.text();
     const signature = req.headers.get("x-cc-webhook-signature");
 
     await connectToDatabase();
@@ -28,12 +91,11 @@ export async function POST(req: NextRequest,res:NextApiResponse) {
     let event = Webhook.verifyEventBody(rawBody, signature, webhookSecret);
     console.log("event", event);
 
-    
     const existingEvent = await prisma.accountInvoices.findFirst({
       where: {
-        coinBaseEventId: event?.id.toString()
-      }
-  });
+        coinBaseEventId: event?.id.toString(),
+      },
+    });
     if (existingEvent) {
       console.log("Duplicate event received. Skipping...");
       return res.status(200).send("Event already processed");
@@ -45,13 +107,13 @@ export async function POST(req: NextRequest,res:NextApiResponse) {
 
     if (event.type === "charge:confirmed") {
       console.log("🚀 ~ charge confirmed", event);
-     
+
       await prisma.accountInvoices.create({
         data: {
           coinBaseEventId: event?.id,
-          invoiceNumber:event.data.name(false, false),
+          invoiceNumber: event.data.name(false, false),
           userId: event.data.metadata.accountDetails,
-          amount: Number( event.data.metadata.amount.replace("$", "")),
+          amount: Number(event.data.metadata.amount.replace("$", "")),
           paymentMethod: "BTC",
           paymentDate: new Date(),
         },
@@ -63,7 +125,57 @@ export async function POST(req: NextRequest,res:NextApiResponse) {
         "user.id"
       );
 
+      const newAccount = await createUserAccount(event.reference);
 
+      await prisma.accountInvoices.updateMany({
+        where: {
+          invoiceId: event.invoiceId,
+        },
+        data: {
+          status: "paid",
+        },
+      });
+
+      const sevenday_cron_job: cronJob = {
+        jobName: `${newAccount.id}_MIN_BET_PERIOD`,
+        time: dateToFullCronString(newAccount.minBetPeriod),
+        type: "objectiveMin",
+        accountId: newAccount.id,
+      };
+      const objectiveMinJob = await fetch(
+        `${process.env.BG_SERVICES_URL}/add-cron-job`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(sevenday_cron_job),
+        }
+      );
+      if (!objectiveMinJob.ok) {
+        throw new Error(await objectiveMinJob.text());
+      }
+
+      // set CRON job for maximum Bet Period
+      const thirtyday_cron_job: cronJob = {
+        jobName: `${newAccount.id}_MAX_BET_PERIOD`,
+        time: dateToFullCronString(newAccount.maxBetPeriod),
+        type: "objectiveMax",
+        accountId: newAccount.id,
+      };
+      const objectiveMaxJob = await fetch(
+        `${process.env.BG_SERVICES_URL}/add-cron-job`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(thirtyday_cron_job),
+        }
+      );
+      if (!objectiveMaxJob.ok) {
+        throw new Error(await objectiveMaxJob.text());
+      }
     }
 
     if (event.type === "charge:failed") {
@@ -73,6 +185,9 @@ export async function POST(req: NextRequest,res:NextApiResponse) {
     return NextResponse.json({ success: true, id: event.id });
   } catch (error) {
     console.error("Error processing webhook:", error);
-    return NextResponse.json({ success: false, error: "Failure to process webhook" }, { status: 400 });
+    return NextResponse.json(
+      { success: false, error: "Failure to process webhook" },
+      { status: 400 }
+    );
   }
 }
